bbc5b641fd020a60a97d68298a74e99c7d775a82
Mohaned Sayed <mohaned.sayed1@gmail.com>
2021-07-16 19:55:12 +0200

    still changing

--- lib/views/landing_page.dart
+++ lib/views/landing_page.dart
@@ -27,90 +27,18 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      floatingActionButton:
-          // FloatingActionButton.extended(
-
-          //   label: Text('sss'),
-          //   isExtended: true,
-          //   backgroundColor: Colors.deepPurple[500],
-          //   onPressed: () {
-          //     modalBottomSheet(context, size);
-          //   },
-          //   // child: Icon(
-          //   //   Icons.add_circle,
-          //   //   color: Colors.white,
-          //   //   size: 38,
-          //   // ),
-          // )
-          SpeedDial(
-        // both default to 16
-        // childMarginTop: 18,
-        marginEnd: 18,
-        marginBottom: 20,
-        // animatedIcon: AnimatedIcons.menu_close,
-        // animatedIconTheme: IconThemeData(size: 22.0),
-        /// This is ignored if animatedIcon is non null
-        icon: Icons.add,
-        activeIcon: Icons.remove,
-        iconTheme: IconThemeData(color: Colors.grey[50], size: 40),
-
-        /// The label of the main button.
-        // label: Text("Open Speed Dial"),
-        /// The active label of the main button, Defaults to label if not specified.
-        // activeLabel: Text("Close Speed Dial"),
-        /// Transition Builder between label and activeLabel, defaults to FadeTransition.
-        // labelTransitionBuilder: (widget, animation) => ScaleTransition(scale: animation,child: widget),
-        /// The below button size defaults to 56 itself, its the FAB size + It also affects relative padding and other elements
-        buttonSize: 56.0,
-        visible: true,
-
-        /// If true user is forced to close dial manually
-        /// by tapping main button and overlay is not rendered.
-        closeManually: false,
-
-        /// If true overlay will render no matter what.
-        renderOverlay: false,
-        curve: Curves.bounceIn,
-        overlayColor: Colors.black,
-        overlayOpacity: 0.5,
-        onOpen: () => print('OPENING DIAL'),
-        onClose: () => print('DIAL CLOSED'),
-        tooltip: 'Post Speed Dial',
-        heroTag: 'speed-dial-hero-tag',
+      floatingActionButton: FloatingActionButton(
+        isExtended: true,
         backgroundColor: primaryColor,
-        foregroundColor: Colors.white,
-        elevation: 8.0,
-        shape: CircleBorder(),
-        // orientation: SpeedDialOrientation.Up,
-        // childMarginBottom: 2,
-        // childMarginTop: 2,
-        children: [
-          SpeedDialChild(
-            child: Icon(Icons.post_add),
-            backgroundColor: Colors.red,
-            label: 'Post',
-            labelStyle: TextStyle(fontSize: 18.0),
-            onTap: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => CreatePost())),
-            onLongPress: () => print('FIRST CHILD LONG PRESS'),
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.photo),
-            backgroundColor: Colors.blue,
-            label: 'Picture',
-            labelStyle: TextStyle(fontSize: 18.0),
-            onTap: () => modalBottomSheet(context, 'Image', true),
-            onLongPress: () => print('SECOND CHILD LONG PRESS'),
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.ondemand_video_outlined),
-            backgroundColor: Colors.green,
-            label: 'Video',
-            labelStyle: TextStyle(fontSize: 18.0),
-            onTap: () => modalBottomSheet(context, 'Video', true),
-            onLongPress: () => print('THIRD CHILD LONG PRESS'),
-          ),
-        ],
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => CreatePost()));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 38,
+        ),
       ),
       key: _scaffoldKey,
       drawer: NavigationDrawerWidget(),
@@ -254,164 +182,10 @@ class _LandingPageState extends State<LandingPage> {
               PostContainer(),
               PostContainer(),
             ],
-            padding: EdgeInsets.only(top: 20, left: 16, right: 16),
+            // padding: EdgeInsets.only(top: 20, left: 16, right: 16),
           ),
         );
       }),
-    );
-  }
-
-  Future modalBottomSheet(BuildContext context, String postType, bool video) {
-    return showModalBottomSheet(
-      backgroundColor: Colors.transparent,
-      context: context,
-      isScrollControlled: true,
-      builder: (context) {
-        return ModalBottomSheet(
-          postType: postType,
-          video: video,
-        );
-      },
-    );
-  }
-}
-
-class ModalBottomSheet extends StatefulWidget {
-  final String postType;
-  final bool video;
-
-  const ModalBottomSheet({Key key, this.postType, this.video})
-      : super(key: key);
-
-  @override
-  _ModalBottomSheetState createState() => _ModalBottomSheetState();
-}
-
-bool pathChosen = false;
-
-var imagePath = '';
-void getImage(ImageSource imageSource) async {
-  final pickedFile = await ImagePicker().getImage(source: imageSource);
-  if (pickedFile != null) {
-    imagePath = pickedFile.path;
-    pathChosen = true;
-  } else {
-    print('no image chosen or taken');
-  }
-}
-
-void getVideo(ImageSource imageSource) async {
-  final pickedFile = await ImagePicker().getVideo(source: imageSource);
-  if (pickedFile != null) {
-    imagePath = pickedFile.path;
-    pathChosen = true;
-  } else {
-    print('no video chosen or taken');
-  }
-}
-
-class _ModalBottomSheetState extends State<ModalBottomSheet> {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height * 0.9,
-      width: size.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(18)),
-            // height: size.height * 0.2,
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.pop(context),
-                    child: Text(
-                      'Cancel',
-                      style: TextStyle(color: Color(0xFF5224E3), fontSize: 18),
-                    ),
-                  ),
-                  GestureDetector(
-                    child: Text(
-                      widget.postType,
-                      style: TextStyle(color: Color(0xFF888888), fontSize: 18),
-                    ),
-                  ),
-                  GestureDetector(
-                    child: Text(
-                      'Post',
-                      style: TextStyle(color: Color(0xFF5224E3), fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Divider(
-            thickness: 2,
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: widget.video == false
-                ? TextField(
-                    cursorColor: Colors.black,
-                    keyboardType: TextInputType.text,
-                    decoration: new InputDecoration.collapsed(
-                        hintText: 'Add question here'),
-                  )
-                : Column(
-                    children: [
-                      TextField(
-                        cursorColor: Colors.black,
-                        keyboardType: TextInputType.text,
-                        decoration: new InputDecoration.collapsed(
-                            hintText: 'Add Caption here'),
-                      ),
-                      Container(
-                        height: size.height * .5,
-                        child: pathChosen == false
-                            ? Center(
-                                child: Text('No image selected'),
-                              )
-                            : Image.file(
-                                File(imagePath),
-                              ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          IconButton(
-                            iconSize: 50,
-                            onPressed: () {
-                              getImage(ImageSource.gallery);
-                            },
-                            icon: Icon(Icons.photo_album),
-                          ),
-                          IconButton(
-                              iconSize: 50,
-                              onPressed: () {
-                                getImage(ImageSource.camera);
-                              },
-                              icon: Icon(
-                                Icons.camera_alt_rounded,
-                              ))
-                        ],
-                      ),
-                    ],
-                  ),
-          ),
-        ],
-      ),
     );
   }
 }
--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -20,12 +20,12 @@ class PostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 20, bottom: 20),
+      margin: EdgeInsets.only(top: 20, left: 16, right: 16, bottom: 20),
       padding: EdgeInsets.all(12),
       decoration: BoxDecoration(
           color: Colors.white,
           boxShadow: [
-            BoxShadow(color: Colors.grey[400], blurRadius: 10),
+            BoxShadow(color: Colors.grey[400], blurRadius: 18),
           ],
           borderRadius: BorderRadius.circular(8)),
       child: Column(
@@ -65,33 +65,39 @@ class PostContainer extends StatelessWidget {
             children: [
               GestureDetector(
                 onTap: () => onAgreePressed,
-                child: Row(
-                  children: [
-                    PostIcon(icon: Icons.thumb_up_alt_outlined),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    PostInteraction(
-                      text: 'Agree',
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Row(
+                    children: [
+                      PostIcon(icon: Icons.thumb_up_alt_outlined),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      PostInteraction(
+                        text: 'Agree',
+                      ),
+                    ],
+                  ),
                 ),
               ),
               VerticalDivider1(),
               GestureDetector(
                 onTap: () => onSharePressed,
-                child: Row(
-                  children: [
-                    PostIcon(
-                      icon: Icons.repeat_rounded,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    PostInteraction(
-                      text: 'Share',
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Row(
+                    children: [
+                      PostIcon(
+                        icon: Icons.share_outlined,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      PostInteraction(
+                        text: 'Share',
+                      ),
+                    ],
+                  ),
                 ),
               ),
               VerticalDivider1(),
@@ -106,24 +112,25 @@ class PostContainer extends StatelessWidget {
                     },
                   );
                 },
-                child: Row(
-                  children: [
-                    PostIcon(icon: Icons.mode_comment_outlined),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    PostInteraction(
-                      text: 'Comment',
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Row(
+                    children: [
+                      PostIcon(icon: Icons.mode_comment_outlined),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      PostInteraction(
+                        text: 'Comment',
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(
-                width: 40,
-              ),
-              PostIcon(
-                icon: Icons.reply_rounded,
-              ),
+              // SizedBox(
+              //   width: 40,
+              // ),
+              Spacer(),
               PostIcon(
                 onPressed: () => showModalBottomSheet(
                   backgroundColor: Colors.transparent,
@@ -137,134 +144,5 @@ class PostContainer extends StatelessWidget {
         ],
       ),
     );
-    // Padding(
-    //   padding: const EdgeInsets.symmetric(vertical: 12.0),
-    //   child: Container(
-    //     decoration: BoxDecoration(
-    //         // gradient: LinearGradient(colors: [Colors.black, Colors.white]),
-    //         color: Colors.white,
-    //         boxShadow: [
-    //           BoxShadow(color: Colors.grey[400], blurRadius: 10),
-    //         ],
-    //         borderRadius: BorderRadius.circular(8)),
-    //     padding: EdgeInsets.all(14),
-    //     // decoration: BoxDecoration(borderRadius:),
-    //     // width: 40,
-    //     // height: MediaQuery.of(context).size.height * 0.25,
-
-    //     child: Column(
-    //       children: [
-    //         Row(
-    //           children: [
-    //             CircleAvatar(
-    //               backgroundImage: NetworkImage(
-    //                   'https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg'),
-    //             ),
-    //             SizedBox(width: 10),
-    //             Column(
-    //               children: [
-    //                 Text(
-    //                   'James jones',
-    //                   style: TextStyle(fontSize: 16),
-    //                 ),
-    //                 SizedBox(
-    //                   height: 4,
-    //                 ),
-    //                 Text(
-    //                   'Today,   03:30',
-    //                   style:
-    //                       TextStyle(fontSize: 12, fontWeight: FontWeight.w200),
-    //                 ),
-    //               ],
-    //             ),
-    //           ],
-    //         ),
-    //         SizedBox(
-    //           height: 20,
-    //         ),
-    //         Text(
-    //             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor.'),
-    //         Spacer(),
-    //         Divider(
-    //           thickness: 1.5,
-    //         ),
-    //         Row(
-    //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //           children: [
-    //             GestureDetector(
-    //               onTap: () => onAgreePressed,
-    //               child: Row(
-    //                 children: [
-    //                   PostIcon(icon: Icons.thumb_up_alt_outlined),
-    //                   SizedBox(
-    //                     width: 5,
-    //                   ),
-    //                   PostInteraction(
-    //                     text: 'Agree',
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //             GestureDetector(
-    //               onTap: () => onSharePressed,
-    //               child: Row(
-    //                 children: [
-    //                   PostIcon(
-    //                     icon: Icons.repeat_rounded,
-    //                   ),
-    //                   SizedBox(
-    //                     width: 5,
-    //                   ),
-    //                   PostInteraction(
-    //                     text: 'Share',
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //             GestureDetector(
-    //               onTap: () {
-    //                 showModalBottomSheet(
-    //                   backgroundColor: Colors.transparent,
-    //                   context: context,
-    //                   isScrollControlled: true,
-    //                   builder: (context) {
-    //                     return CommentContainer(onAgreePressed: onAgreePressed);
-    //                   },
-    //                 );
-    //               },
-    //               child: Row(
-    //                 children: [
-    //                   PostIcon(
-    //                     icon: Icons.comment_rounded,
-    //                   ),
-    //                   SizedBox(
-    //                     width: 5,
-    //                   ),
-    //                   PostInteraction(
-    //                     text: 'Comment',
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //             SizedBox(
-    //               width: 40,
-    //             ),
-    //             PostIcon(
-    //               icon: Icons.reply_rounded,
-    //             ),
-    //             PostIcon(
-    //               onPressed: () => showModalBottomSheet(
-    //                 backgroundColor: Colors.transparent,
-    //                 context: context,
-    //                 builder: (context) => ThreeDots(),
-    //               ),
-    //               icon: Icons.more_horiz,
-    //             ),
-    //           ],
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
